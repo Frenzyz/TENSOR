086f3d9e194b77ace53c3333f5a21725f02a8f8e
Aadit Chetan <Aadister16@gmail.com>
2025-02-04 02:31:59 -0500

    Capacitor Support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tensor.app',
+  appName: 'Tensor',
+  webDir: 'public'
+};
+
+export default config;
